20f62f84d8c0d1f3bb08a8b91c021b6d70eb7526
Harmehar-kaur <kaurharmehar9717@gmail.com>
2023-12-16 13:36:31 +0530

    first-page-front-end

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+// git remote add origin https://github.com/Harmehar-kaur/brewStack.git
+// git branch -M main
+// git push -u origin main
+
 // requiring the express lib
 const express = require('express');
 const app = express(); //server set up and routing 
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
 
 app.use(express.urlencoded());
 
 app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 // set up the view engine
 app.set('view engine', 'ejs'); //easy rendering 
