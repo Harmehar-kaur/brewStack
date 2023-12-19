f815d4858808b90268f1b2e496ae63a637c92c45
Harmehar-kaur <kaurharmehar9717@gmail.com>
2023-12-19 13:41:19 +0530

    maximum things done

---
+++ controllers/service_controller.js
@@ -0,0 +1,47 @@
+module.exports.order = function(req,res){
+    return res.render('coffeespecial',{
+        title:'Brew Stack | Order'
+      })
+}
+
+module.exports.coffee = function(req,res){
+    return res.render('coffeespecial',{
+        title:'Brew Stack | Order'
+      })
+}
+
+module.exports.speciality = function(req,res){
+    return res.render('coffeespecial',{
+        title:'Brew Stack | Order'
+      })
+}
+
+module.exports.tea = function(req,res){
+    return res.render('coffeespecial',{
+        title:'Brew Stack | Order'
+      })
+}
+
+module.exports.snacks = function(req,res){
+    return res.render('coffeespecial',{
+        title:'Brew Stack | Order'
+      })
+}
+
+module.exports.deserts = function(req,res){
+    return res.render('coffeespecial',{
+        title:'Brew Stack | Order'
+      })
+}
+
+module.exports.refreshments = function(req,res){
+    return res.render('coffeespecial',{
+        title:'Brew Stack | Order'
+      })
+}
+
+module.exports.kids = function(req,res){
+    return res.render('coffeespecial',{
+        title:'Brew Stack | Order'
+      })
+}
--- routes/services.js
+++ routes/services.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const serviceController = require('../controllers/service_controller');
 
 
 
+router.get('/',serviceController.order);
+router.get('/coffee',serviceController.coffee); 
+router.get('/speciality',serviceController.speciality); 
+router.get('/tea',serviceController.tea); 
+router.get('/snacks',serviceController.snacks); 
+router.get('/deserts',serviceController.deserts); 
+router.get('/refreshments',serviceController.refreshments); 
+router.get('/kids',serviceController.kids); 
 
-router.get('/', (req, res) => {
-  res.render('order',{
-    title:'Brew Stack | Order'
-  })
-})
 
 
 
